aa6a89179b623fcd8d71488fc80238844036cee2
JaimeBotet <jaime.90.botet@gmail.com>
2021-02-01 10:48:53 +0100

    First commit

---
+++ hello.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"time"
+)
+
+func f(from string,i int) {
+	fmt.Println(from, " Sleeping...", i)
+	time.Sleep(time.Second)
+}
+
+
+func main() {
+	// create a new 'ServeMux'
+	mux := http.NewServeMux() 
+	//Handle '/' route
+	mux.HandleFunc("/sync", func(res http.ResponseWriter, req *http.Request) {
+		for i := 0; i < 3; i++ {
+			f("sync", i)
+		}
+		fmt.Fprint(res, "Finished sync request")
+	})
+
+	//Handle '/hello/golang' route
+	mux.HandleFunc("/go/async", func(res http.ResponseWriter, req *http.Request) {
+		for i := 0; i < 3; i++ {
+			go f("async",i)
+		}
+		fmt.Fprint(res, "Finished async request")
+	})
+	
+    // listen and serve using 'ServeMux'
+    http.ListenAndServe(":9000", mux)
+}
